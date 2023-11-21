4bd7162fc360171685ebcbeb5b1064335783e9a1
Mirek Simek <miroslav.simek@gmail.com>
2023-11-21 19:17:01 +0100

    Fixing tests

--- tests/test_import_export.py
+++ tests/test_import_export.py
@@ -2,17 +2,20 @@ import tempfile
 
 from invenio_vocabularies.records.api import Vocabulary
 from oarepo_runtime.datastreams.fixtures import dump_fixtures, load_fixtures
-from oarepo_runtime.uow import BulkUnitOfWork
+from oarepo_runtime.datastreams.types import StatsKeepingDataStreamCallback
 
 
 def test_complex_import_export(app, db, cache, search_clear, vocab_cf):
-    result = load_fixtures(batch_size=100)
-    assert result.failed_count == 0
-    assert result.skipped_count == 0
-    assert len(result.results) == 10
+    load_callback = StatsKeepingDataStreamCallback()
+    load_fixtures(batch_size=100, callback=load_callback)
+    assert load_callback.failed_entries_count == 0
+    assert load_callback.filtered_entries_count == 0
+    assert load_callback.ok_entries_count == 2713
     Vocabulary.index.refresh()
+
     with tempfile.TemporaryDirectory() as d:
-        dump_result = dump_fixtures(d)
-        assert dump_result.failed_count == 0
-        assert dump_result.skipped_count == 0
-        assert dump_result.ok_count == result.ok_count
+        dump_callback = StatsKeepingDataStreamCallback()
+        dump_fixtures(d, callback=dump_callback)
+        assert dump_callback.failed_entries_count == 0
+        assert dump_callback.filtered_entries_count == 0
+        assert dump_callback.ok_entries_count == load_callback.ok_entries_count
